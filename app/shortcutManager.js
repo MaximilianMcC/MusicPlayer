84185e0275ba59f1b60c6a6326792effcd112086
Max <max@maximilian.co.nz>
2023-03-18 15:51:20 +1300

    Added play/pause shortcut

--- app/shortcutManager.js
+++ app/shortcutManager.js
@@ -1,6 +1,24 @@
 // Check for if a key is pressed
 document.addEventListener("keydown", (e) => {
 
-    
+    // Check for what key is pressed
+    //TODO: Make this customizable in settings
+    switch (e.key) {
+
+        case " ":
+            // Play/pause using space
+            playPauseButton.click();
+            break;
+        
+        case "ArrowRight":
+            //TODO: Skip 10 seconds ahead in the song
+            break;
+        
+        case "ArrowLeft":
+            //TODO: Go back 10 seconds in the song
+            break;
+        
+        
+    }
 
 });
