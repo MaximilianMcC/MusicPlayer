06ee90c34b627c4cc215b3fe89d57056aabfdc2f
Max <max@maximilian.co.nz>
2023-07-07 16:57:10 +1200

    Bars freeze if the audio is paused

--- app/visualizer.js
+++ app/visualizer.js
@@ -18,6 +18,9 @@ analyser.fftSize = 256;
 const barCount = 18;
 const audioData = new Uint8Array(barCount);
 
+// Store the previous data so that it can be displayed when the music is paused
+const previousAudioData = audioData;
+
 // Bar settings
 const barColor = "transparent";
 const backgroundColor = "#081926";
@@ -31,15 +34,17 @@ function renderFrame() {
 	// Get the frequency data from the analyser
 	analyser.getByteFrequencyData(audioData);
 	
+	// Check for if the audio is paused, and if it is then get the previous audio data
+	if (audioPlayer.paused) audioData = previousAudioData;
+
 	// Clear and reset the canvas
-	// canvasContext.clearRect(0, 0, width, height);
 	canvasContext.fillStyle = backgroundColor;
 	canvasContext.fillRect(0, 0, width, height);
 	x = 0;
 
 	// Loop over every bar and draw it
 	for (let i = 0; i < barCount; i++) {
-		
+
 		// Get the height of the bar and calculate its Y position
 		barHeight = audioData[i] * 0.5; // 0.5 makes the things smaller so its not just one huge block
 		let y = height - barHeight;
